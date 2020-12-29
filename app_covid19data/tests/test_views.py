e4bd945d5dfc134dbd7d2e4846a339841c152ccf
Richi <ricardorg20@gmail.com>
2020-12-29 15:12:02 +0100

    Some tests added

--- app_covid19data/tests/test_views.py
+++ app_covid19data/tests/test_views.py
@@ -9,13 +9,21 @@ from app_covid19data import views
 
 class Covid19dataTest(TestCase):
 
+    def setUp(self):
+        """ Method which the testing framework will automatically call before every single test we run """
+        # Create several rows
+        self.datacovid19 = baker.make(DataCovid19Item, country='Spain', date=timezone.now().date(),
+                                      dead_cases=1, confirmed_cases=1, recovered_cases=1,
+                                      _quantity=5)
+
     # Views tests
     def test_covid19data_resume_view(self):
-        # First create several rows
-        w = baker.make(DataCovid19Item, country='Spain', date=timezone.now().date(),
-                       dead_cases=1, confirmed_cases=1, recovered_cases=1,
-                       _quantity=5)
         url = reverse(views.resume_view)
         resp = self.client.get(url)
         self.assertEqual(resp.status_code, 200)
         # self.assertIn(w.title, resp.content)
+
+    def test_covid19data_get_resume_country(self):
+        # The data for the test are loading in above setUp function
+        queryset = views.get_resume_country('Spain')
+        self.assertEqual(queryset['country'], 'Spain')
